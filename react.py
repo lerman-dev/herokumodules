a94a2c59acb2b7cd8f4dab2cc8177d485e249971
lerman-dev <freerobux.tcheburek@yandex.ru>
2026-02-23 21:36:59 +0600

    Update react.py

--- react.py
+++ react.py
@@ -1,10 +1,11 @@
 from .. import loader, utils
 from telethon.tl.functions.messages import SendReactionRequest
 from telethon.tl.types import ReactionEmoji
+import asyncio
 
 @loader.tds
 class PoopReactorMod(loader.Module):
-    """Ставит 💩 по команде"""
+    """💩 реактор с массовым режимом"""
 
     strings = {"name": "PoopReactor"}
 
@@ -13,17 +14,33 @@ class PoopReactorMod(loader.Module):
         self.enabled = False
 
     async def poopcmd(self, message):
-        """вкл/выкл 💩 режим"""
+        """вкл/выкл авто 💩"""
         self.enabled = not self.enabled
-
         status = "включен 💩" if self.enabled else "выключен 🚫"
         await utils.answer(message, f"Режим {status}")
 
+    async def poopallcmd(self, message):
+        """💩 на ВСЕ сообщения"""
+        await utils.answer(message, "Начинаю 💩-геноцид... держись 😈")
+
+        async for msg in message.client.iter_messages(self.chat_id):
+            try:
+                await message.client(
+                    SendReactionRequest(
+                        peer=self.chat_id,
+                        msg_id=msg.id,
+                        reaction=[ReactionEmoji(emoticon="💩")],
+                        big=False
+                    )
+                )
+                await asyncio.sleep(0.5)  # анти-бан пауза
+            except:
+                await asyncio.sleep(1)
+
+        await utils.answer(message, "💩 Готово. Чат официально испорчен.")
+
     async def watcher(self, message):
         if not self.enabled:
-            return
-
-        if not message or not message.chat_id:
             return
 
         if message.chat_id != self.chat_id:
@@ -32,11 +49,14 @@ class PoopReactorMod(loader.Module):
         try:
             await message.client(
                 SendReactionRequest(
-                    peer=message.chat_id,
+                    peer=self.chat_id,
                     msg_id=message.id,
                     reaction=[ReactionEmoji(emoticon="💩")],
                     big=False
                 )
+            )
+        except:
+            pass            )
             )
         except:
             pass
