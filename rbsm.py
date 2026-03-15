fb79b721e598a73b31d53ffab7a084b0fc50b626
lerman-dev <freerobux.tcheburek@yandex.ru>
2026-03-16 00:34:42 +0600

    Update rbsm.py

--- rbsm.py
+++ rbsm.py
@@ -1,13 +1,13 @@
 from .. import loader, utils
 
 @loader.tds
-class KarmaMod(loader.Module):
-    """Изменение кармы"""
+class KarmaBanMod(loader.Module):
+    """Карма и фейк бан"""
 
-    strings = {"name": "KarmaMod"}
+    strings = {"name": "KarmaBan"}
 
     async def givekarmacmd(self, message):
-        """Использование: .givekarma <число> (ответом на сообщение)"""
+        """.givekarma <число> (ответом на сообщение)"""
 
         args = utils.get_args_raw(message)
 
@@ -21,20 +21,40 @@ class KarmaMod(loader.Module):
 
         reply = await message.get_reply_message()
         if not reply:
-            return await utils.answer(message, "⚠️ Команда должна быть ответом на сообщение")
+            return await utils.answer(message, "⚠️ Ответь на сообщение пользователя")
 
         giver = await message.get_sender()
         target = await reply.get_sender()
 
-        giver_name = giver.first_name
-        target_name = target.first_name
-
-        giver_link = f'<a href="tg://user?id={giver.id}">{giver_name}</a>'
-        target_link = f'<a href="tg://user?id={target.id}">{target_name}</a>'
+        giver_link = f'<a href="tg://user?id={giver.id}">{giver.first_name}</a>'
+        target_link = f'<a href="tg://user?id={target.id}">{target.first_name}</a>'
 
         text = (
             f"🔰 {giver_link} изменил карму на <b>{value}</b> пользователю {target_link}\n"
             f"🔰 Новое значение кармы: <b>{value}</b>"
         )
 
-        await utils.answer(message, text)
+        await reply.reply(text)
+
+    async def bancmd(self, message):
+        """.ban <причина> (ответом на сообщение)"""
+
+        reason = utils.get_args_raw(message) or "не указана"
+
+        reply = await message.get_reply_message()
+        if not reply:
+            return await utils.answer(message, "⚠️ Ответь на сообщение нарушителя")
+
+        admin = await message.get_sender()
+        target = await reply.get_sender()
+
+        admin_link = f'<a href="tg://user?id={admin.id}">{admin.first_name}</a>'
+        target_link = f'<a href="tg://user?id={target.id}">{target.first_name}</a>'
+
+        text = (
+            f"🚫 {target_link} получил(а) бан навсегда\n"
+            f"👮 Администратор: {admin_link}\n"
+            f"📝 Причина: <b>{reason}</b>"
+        )
+
+        await reply.reply(text)
